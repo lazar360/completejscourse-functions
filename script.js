a933236c0343dbf47495129d46ea695badeb76f9
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-06-26 09:56:54 +0200

    maj script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function(flightNum, numPasengers = 1, price = 199){
+
+    const booking = {
+        flightNum,
+        numPasengers,
+        price
+    }
+
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('LH123');
