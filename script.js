5bd9c59b1ef4fa1b0d415854dcf00188a17b384e
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-06-27 09:27:16 +0200

    maj script

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 'use strict';
 
-const bookings = [];
+// DEFAULT ARUMENT
+// const bookings = [];
 
-const createBooking = function (flightNum, numPasengers = 1, price = 199) {
-  const booking = {
-    flightNum,
-    numPasengers,
-    price,
-  };
+// const createBooking = function (flightNum, numPasengers = 1, price = 199) {
+//   const booking = {
+//     flightNum,
+//     numPasengers,
+//     price,
+//   };
 
-  console.log(booking);
-  bookings.push(booking);
-};
+//   console.log(booking);
+//   bookings.push(booking);
+// };
 
-createBooking('LH123', 2, 800);
-createBooking('LH123', undefined, 800); // skip a default argument
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', undefined, 800); // skip a default argument
+
+// VALUE VS REFERENCE
+
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas',
+    passport: 24739479284
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum ='LH9999';
+    passenger.name = 'Mr ' + passenger.name;
+
+    if(passenger.passport === 24739479284) {
+        alert('Passenger checked');
+    } else {
+        alert('Passenger not checked');
+    }
+}
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// il faut faire un essai avec un objet complexe
+// créer un objet complese : avec une liste d'objet
+// faire un test sur la liste d'objets et changer l'objet (ajouter une propriété)
+// loguer l'objet 
