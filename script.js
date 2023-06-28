3e0b4eea6731dc6822d93c584f3aacb719b50c3b
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-06-28 14:16:06 +0200

    maj script

--- script.js
+++ script.js
@@ -19,27 +19,43 @@
 
 // VALUE VS REFERENCE
 
-const flight = 'LH234';
-const jonas = {
-    name: 'Jonas',
-    passport: 24739479284
-}
+// const flight = 'LH234';
+// const jonas = {
+//     name: 'Jonas',
+//     passport: 24739479284
+// }
 
-const checkIn = function(flightNum, passenger) {
-    flightNum ='LH9999';
-    passenger.name = 'Mr ' + passenger.name;
+// const checkIn = function(flightNum, passenger) {
+//     flightNum ='LH9999';
+//     passenger.name = 'Mr ' + passenger.name;
 
-    if(passenger.passport === 24739479284) {
-        alert('Passenger checked');
-    } else {
-        alert('Passenger not checked');
-    }
-}
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
+//     if(passenger.passport === 24739479284) {
+//         alert('Passenger checked');
+//     } else {
+//         alert('Passenger not checked');
+//     }
+// }
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
 
-// il faut faire un essai avec un objet complexe
-// créer un objet complese : avec une liste d'objet
-// faire un test sur la liste d'objets et changer l'objet (ajouter une propriété)
-// loguer l'objet 
+// functions accepting callback functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best', upperFirstWord);
+transformer('Javascript is the best', oneWord);
