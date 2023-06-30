62a9796bb5cd8367a8170532ef85646cb49c97b0
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-06-30 09:04:28 +0200

    maj script

--- script.js
+++ script.js
@@ -85,43 +85,64 @@
 
 // THE CALL AND APPLY METHODS
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline}, flight ${this.iataCode} ${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
-};
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline}, flight ${this.iataCode} ${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
 
-lufthansa.book(239, 'Nicolas');
-lufthansa.book(365, 'John');
+// lufthansa.book(239, 'Nicolas');
+// lufthansa.book(365, 'John');
 
-const eurowings = {
-  name: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+// const eurowings = {
+//   name: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
 
-const book = lufthansa.book;
+// const book = lufthansa.book;
 
 // Does NOT work
 // book(23, 'John');
 
-book.call(eurowings, 23, 'Sarah Connors');
+//book.call(eurowings, 23, 'Sarah Connors');
 
 // for an array
 //book.call(eurowings, ...flightData);
 
 // BIND METHOD
 
-const bookEW = book.bind(eurowings);
-const bookLH = book.bind(lufthansa);
-bookEW(23, 'Steven Williams');
-bookLH(23, 'John');
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+// bookEW(23, 'Steven Williams');
+// bookLH(23, 'John');
 
-const bookEW23 = book.bind(eurowings, 23);
-bookEW23('John');
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('John');
+
+// with event listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// IMMEDIATELY INVOKED FUNCTIONS EXPRESSIONS (function(){})()
+
+(function(){
+  console.log('This will never run again');
+})();
+
+(() => console.log('This will ALSO never run again'))();
+
